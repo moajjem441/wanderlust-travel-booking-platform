0450b111d6e1d010c102c3b7f69c7795c2fbc605
Moajjem Hossain <moajjem441@gmail.com>
2026-05-14 10:41:37 +0600

    40% alomost done

---
+++ wanderlast-server/index.js
@@ -0,0 +1,108 @@
+const express =require("express");
+const app = express();
+const cors=require('cors');
+const dotenv=require("dotenv");
+
+dotenv.config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri =process.env.MONG_AUTH;
+const port =process.env.PORT 
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //database and collection create
+    const db=client.db("wanderlust")
+    const destinationCollection = db.collection("destination")
+
+
+    //get api for collecting data from database
+    app.get('/destination',async(req,res)=>{
+        const result = await destinationCollection.find().toArray();
+        res.json(result)
+    })
+
+
+   
+    //api create for destination
+   app.post('/destination',async(req,res)=>{
+    const destinationData = req.body
+     console.log(destinationData)
+    const result = await destinationCollection.insertOne(destinationData)
+    res.json(result)
+    console.log(result)
+   })
+
+
+
+   //get api call for collecting destination details data
+   app.get('/destination/:id',async(req,res)=>{
+    const {id}=req.params;
+    const result =await destinationCollection.findOne({_id: new ObjectId(id)});
+    res.json(result);
+   })
+
+
+
+   //patch api call for edit data
+   app.patch('/destination/:id',async(req,res)=>{
+    const {id}=req.params;
+    const updateDestination=req.body;
+    const result= await destinationCollection.updateOne({_id: new ObjectId(id)},{$set:updateDestination})
+    res.json(result);
+   })
+
+
+   //delete api for delete destinaiton data
+   app.delete('/destination/:id',async(req,res)=>{
+    const {id}=req.params
+    const result = await destinationCollection.deleteOne({_id: new ObjectId(id)})
+    res.json(result)
+   })
+
+
+   //post for send mail and passs
+   
+
+    
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Server is runnig perfectly")
+})
+
+
+app.listen(port,(req,res)=>{
+    console.log(`Server is running on Port${port}`)
+})
+
+
+
