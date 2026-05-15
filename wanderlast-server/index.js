df917c41285330e95fcb0cd244158d902e9813b0
Moajjem Hossain <moajjem441@gmail.com>
2026-05-16 00:10:47 +0600

    booking delete part updated

--- wanderlast-server/index.js
+++ wanderlast-server/index.js
@@ -99,6 +99,13 @@ async function run() {
   })
    
 
+
+  app.delete("/bookings/:bookingId",async(req,res)=>{
+    const {bookingId}=req.params
+    const result =await bookingCollection.deleteOne({_id: new ObjectId(bookingId)})
+    res.json(result)
+  })
+
     
 
 
